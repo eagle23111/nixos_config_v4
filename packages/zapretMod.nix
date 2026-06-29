1176e8ee20dcca5075cbd3057eb57c269305dfd7
eagle23111 <stasapohta@yandex.ru>
2026-06-29 20:14:16 +0300

    update

--- packages/zapretMod.nix
+++ packages/zapretMod.nix
@@ -9,7 +9,7 @@
         url = "https://raw.githubusercontent.com/Flowseal/zapret-discord-youtube/22a9ca3bc067441789c83bd80c00d38e54be51c4/bin/tls_clienthello_max_ru.bin";
         sha256 = "TuCHCr4KAShgCwCVGJmHuh0hDa6L+WO8clr/Sc+SJiQ=";
       };
-      stub = pkgs.fetchurl {
+      stun = pkgs.fetchurl {
         url = "https://raw.githubusercontent.com/Flowseal/zapret-discord-youtube/22a9ca3bc067441789c83bd80c00d38e54be51c4/bin/stun.bin";
         sha256 = "nNVGkwl4DKVsC9lyZlJKSMfuUp0Cwxec/ssgsmCllkE=";
       };
@@ -22,7 +22,7 @@
         ${oldAttrs.installPhase}
         # mkdir -p $out/usr/share/zapret/files/fake
         cp ${tlsClientHelloMax} $out/usr/share/zapret/files/fake/tls_clienthello_max_ru.bin
-        cp ${stub} $out/usr/share/zapret/files/fake/stub.bin
+        cp ${stun} $out/usr/share/zapret/files/fake/stun.bin
         cp ${darkcloud} $out/usr/share/zapret/files/fake/quic_initial_dbankcloud_ru.bin
       '';
     });
