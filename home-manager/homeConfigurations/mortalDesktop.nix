485b3039608b346acbfa99b4e00789cafdc3e711
eagle23111 <stasapohta@yandex.ru>
2026-08-16 14:46:18 +0300

    update

--- home-manager/homeConfigurations/mortalDesktop.nix
+++ home-manager/homeConfigurations/mortalDesktop.nix
@@ -14,7 +14,7 @@
       inputs.zen-browser-flake.homeModules.default
 
       self.homeModules.zsh
-      self.homeModules.hyprland
+      #self.homeModules.hyprland
       self.homeModules.mortalDesktopModule
       self.homeModules.zen-browser
 
