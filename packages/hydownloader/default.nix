e38e13707e23f771fc3f3169395c51938fb8c31f
eagle23111 <stasapohta@yandex.ru>
2026-09-12 18:59:09 +0300

    update

--- packages/hydownloader/default.nix
+++ packages/hydownloader/default.nix
@@ -1,10 +1,10 @@
 {
   pkgs,
   lib,
-  python313,
+  python314,
   gallery-dl,
 }: let
-  python = python313;
+  python = python314;
 in
   python.pkgs.buildPythonApplication {
     pname = "hydownloader";
@@ -16,17 +16,24 @@ in
       ref = "master";
     };
 
+    build-system = [
+      python.pkgs.poetry-core
+    ];
+
     nativeBuildInputs = [
       python.pkgs.poetry-core
     ];
+
     postPatch = ''
       substituteInPlace pyproject.toml \
         --replace-fail 'gallery-dl (>=1.32.8,<2.0.0)' 'gallery-dl' \
         --replace-fail 'click (>=8.3.2,<9.0.0)' 'click' \
         --replace-fail 'hydrus-api (>=5.2.1,<6.0.0)' 'hydrus-api' \
-        --replace-fail 'poetry-core>=2.0.0,<3.0.0' 'poetry-core'
+        --replace-fail 'poetry-core>=2.0.0,<3.0.0' 'poetry-core' \
+        --replace-fail 'curl-cffi (>=0.15.0,<0.16.0)' 'curl-cffi'
     '';
-    propagatedBuildInputs = [
+
+    propagatedBuildInputs = [      
       python.pkgs.click
       python.pkgs.bottle
       python.pkgs.yt-dlp
@@ -42,7 +49,13 @@ in
       python.pkgs.requests
       python.pkgs.cheroot
       python.pkgs.brotli
-      gallery-dl
+      
+      # Override gallery-dl to use the same python314 package set and yt-dlp derivation
+      (gallery-dl.override {
+        python3Packages = python.pkgs;
+        yt-dlp = python.pkgs.yt-dlp;
+      })
+      
       python.pkgs.pillow
       python.pkgs.pysocks
       python.pkgs.yt-dlp-ejs
